2bcbaff378c830f095aae1e58e90f46c1471d522
Andy Lin <ld130101@gmail.com>
2023-03-10 18:11:04 +0800

    socket server connect

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+    methods: ["GET", 'POST'],
+  },
+});
 
+io.on('connection', (socket) => {
+  //connect
+  console.log('a user connected !');
+  io.emit('welcome', 'Hello socket!');
+  socket.on('hello', (arg) => console.log(arg))
+})
 
 const port = process.env.PORT || 6001;
-app.listen(port, () => console.log(`server started, port: ${port}`))
+http.listen(port, () => console.log(`Socket server started, port: ${port}`))
